f6a29a1e35b1ed443f814e1b0bcb0d1ce94dec08
flawedworld <flawedworld@flawed.world>
2022-10-15 19:25:55 +0100

    Add sysconfig from stock OS
    Co-Authored-By: Vladislav <stvlad06@gmail.com>

--- device-lynx.mk
+++ device-lynx.mk
@@ -31,6 +31,10 @@ $(call soong_config_set,lyric,camera_hardware,lynx)
 $(call soong_config_set,lyric,tuning_product,lynx)
 $(call soong_config_set,google3a_config,target_device,lynx)
 
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
+
 # Init files
 PRODUCT_COPY_FILES += \
 	device/google/lynx/conf/init.lynx.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.lynx.rc
