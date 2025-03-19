e526d645894d42dbc67ddd4f0f603f72650a4aaf
ZirgomHaidar <zirgomhaidar17@gmail.com>
2025-03-19 22:40:58 +0530

    Explicitly declare KERNEL properties

--- device-lynx.mk
+++ device-lynx.mk
@@ -14,10 +14,9 @@
 # limitations under the License.
 #
 
-TARGET_LINUX_KERNEL_VERSION := $(RELEASE_KERNEL_LYNX_VERSION)
-# Keeps flexibility for kasan and ufs builds
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_LYNX_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_LYNX_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/lynx-kernels/6.1/25Q1-13202328
+TARGET_BOARD_KERNEL_HEADERS := device/google/lynx-kernels/6.1/25Q1-13202328/kernel-headers
+TARGET_PREBUILT_KERNEL := device/google/lynx-kernels/6.1/25Q1-13202328/Image.lz4
 
 DEVICE_PACKAGE_OVERLAYS += device/google/lynx/lynx/overlay
 
